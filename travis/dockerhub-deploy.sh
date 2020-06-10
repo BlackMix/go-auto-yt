37cf698b181344affb9037f5a9e5765ce8e752bf
Mix <mixchatmix3@gmail.com>
2020-06-10 13:39:53 -0300

    Update dockerhub-deploy.sh

--- travis/dockerhub-deploy.sh
+++ travis/dockerhub-deploy.sh
@@ -4,12 +4,8 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
-
-    docker manifest create ${DOCKER_USER}/ytd2:latest \
-            ${DOCKER_USER}/ytd2:latest-arm64
-
-    docker manifest annotate ${DOCKER_USER}/ytd2:latest ${DOCKER_USER}/ytd2:latest-arm64 --arch arm64
-
+    docker manifest create ${DOCKER_USER}/ytd2:latest
+    docker manifest annotate ${DOCKER_USER}/ytd2:latest --arch arm64
     docker manifest push ${DOCKER_USER}/ytd2:latest
 else
     docker images
