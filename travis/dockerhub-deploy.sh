13271caed23765b7d3930118cda33cc7ee3ae8ae
Mix <mixchatmix3@gmail.com>
2020-06-10 13:17:23 -0300

    ytd2

--- travis/dockerhub-deploy.sh
+++ travis/dockerhub-deploy.sh
@@ -5,12 +5,12 @@ export DOCKER_CLI_EXPERIMENTAL=enabled
 if [ "${TRAVIS_PULL_REQUEST}" = "false" ]; then
     echo $DOCKER_PASSWORD | docker login -u $DOCKER_USER --password-stdin
 
-    docker manifest create ${DOCKER_USER}/go-auto-yt:latest \
-            ${DOCKER_USER}/go-auto-yt:latest-arm64
+    docker manifest create ${DOCKER_USER}/ytd2:latest \
+            ${DOCKER_USER}/ytd2:latest-arm64
 
-    docker manifest annotate ${DOCKER_USER}/go-auto-yt:latest ${DOCKER_USER}/go-auto-yt:latest-arm64 --arch arm64
+    docker manifest annotate ${DOCKER_USER}/ytd2:latest ${DOCKER_USER}/ytd2:latest-arm64 --arch arm64
 
-    docker manifest push ${DOCKER_USER}/go-auto-yt:latest
+    docker manifest push ${DOCKER_USER}/ytd2:latest
 else
     docker images
 fi
